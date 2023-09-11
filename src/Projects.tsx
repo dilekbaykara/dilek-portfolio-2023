8bdcab48db648d42ca2003b4cfa9d2bbbdeade83
Dilek Baykara <baykara.d@gmail.com>
2023-09-11 16:59:18 -0400

    adjusted nav bar checkbox

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -7,33 +7,31 @@ function Projects() {
       <div id="project-title">Projects</div>
       <div id="projects-div">
         <div className="project-box">
-          <div className="project-div-1">
-            <div className="project-name">eBird App</div>
-            <div className="project-lang">
-              Node.js, Puppeteer, Leaflet.js, and Typescript
-            </div>
-            <div className="project-info">
-              <p>
-                - Built a project for birders to view their birdwatching
-                sessions on one map{" "}
-              </p>{" "}
-              <p>
-                {" "}
-                - eBird crawler crawls the Cornell database and iterates through
-                checklist pages.
-              </p>{" "}
-              <p>
-                {" "}
-                - Map data is then extracted and concatenated into a single
-                large map using Leaflet.js.
-              </p>{" "}
-              <p>
-                {" "}
-                - Map pins display information for species observed during
-                session
-              </p>
-            </div>
+          <div className="project-name">eBird App</div>
+          <div className="project-lang">
+            Node.js, Puppeteer, Leaflet.js, and Typescript
           </div>
+          <div className="project-info">
+            <p>
+              - Built a project for birders to view their birdwatching sessions
+              on one map{" "}
+            </p>{" "}
+            <p>
+              {" "}
+              - eBird crawler crawls the Cornell database and iterates through
+              checklist pages.
+            </p>{" "}
+            <p>
+              {" "}
+              - Map data is then extracted and concatenated into a single large
+              map using Leaflet.js.
+            </p>{" "}
+            <p>
+              {" "}
+              - Map pins display information for species observed during session
+            </p>
+          </div>
+
           <div id="img-div">
             <img alt="project-preview" src={eBirdImage} id="e-bird-img"></img>
           </div>
